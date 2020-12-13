5bc2df9e54df6bd3c0c7c1f028836c692d58d8ac
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2020-12-13 08:53:56 +0000

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blogs
   resources :users, only: [:new, :create, :show]
 end
